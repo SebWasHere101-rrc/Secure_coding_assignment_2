7d98df76d7907a8df5570fe6161be0c8b8390dde
Sebastian <svileta@academic.rrc.ca>
2025-11-26 09:16:32 -0600

    I have edit something for java

--- assignment1_code_sample_3.java
+++ assignment1_code_sample_3.java
@@ -10,7 +10,7 @@ import java.util.Scanner;
 public class VulnerableApp {
 
     private static final String DB_URL = "jdbc:mysql://mydatabase.com/mydb";
-    private static final String DB_USER = "admin";
+    private static final String DB_USER = "admin_1";
     private static final String DB_PASSWORD = "secret1234";
 
     public static String getUserInput() {
